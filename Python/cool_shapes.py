bf70738eddf31cb848a92274e0a3983866720c9d
Vanshika Rathod <85212298+vanshikaaaaaaaa@users.noreply.github.com>
2024-10-16 19:44:02 +0530

    Update cool_shapes.py
    added a file

--- Python/cool_shapes.py
+++ Python/cool_shapes.py
@@ -8,6 +8,29 @@ hideturtle()
 circle(5*5.2)
 color('red')
 done()
+
+# -----------------ALTERNATIVE CODE--------
+
+import turtle
+
+# Setup screen
+screen = turtle.Screen()
+screen.bgcolor("white")
+
+# Setup turtle
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.speed(5)
+
+# Draw a cool spiral pattern
+colors = ["red", "yellow", "blue", "green", "purple", "orange"]
+for i in range(50):
+    my_turtle.pencolor(colors[i % 6])
+    my_turtle.forward(i * 10)
+    my_turtle.right(144)  # This angle creates a star-like spiral
+
+turtle.done()
+
 # for i in range(1):
 #     color('red')
 #     circle(i)
